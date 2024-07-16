841461ac9456db988413985403478d58eb4e20d9
Ivans Karšenieks <ivansk@metropolia.fi>
2024-07-16 18:01:34 +0300

    fixed pwm

--- Core/Src/commands.c
+++ Core/Src/commands.c
@@ -12,12 +12,12 @@
 
 void set_pwm_duty_cycle(TIM_HandleTypeDef *htim){
 	if(check_bit(PWM_out_enable,1) != 0){
-		PWM_speed[0] = (PWM_width[0] * 40)/100;
+		PWM_speed[0] = (PWM_width[0] * 1000)/100;
 	}else{
 		PWM_speed[0] = 0;
 	}
-	if(check_bit(PWM_out_enable,0) != 0){
-		PWM_speed[1] = (PWM_width[1] * 40)/100;
+	if(check_bit(PWM_out_enable,0) != 0){ //duty cycle only 0% 20% 40% 60% 80% and 100%
+		PWM_speed[1] = (PWM_width[1])/20;
 	}else{
 		PWM_speed[1] = 0;
 	}
